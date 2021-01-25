c5d7824fc0994584b8cd1004546798201ccbb759
Rustam Nortadzhiev <rustamnortojiyev@gmail.com>
2021-01-25 16:31:09 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,22 +1,138 @@
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
-
+import ListItem from './ListItem';
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [currentItem, setCurrentItem] = React.useState({ text: '', key: null });
+  const [status, setStatus] = React.useState('uncompleted');
+  const [filteredTodos, setFiltredTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const getLocalTodos = () => {
+      if (localStorage.getItem('items') === null) {
+        localStorage.setItem('items', JSON.stringify([]));
+      } else {
+        let localTodos = JSON.parse(localStorage.getItem('items'));
+        setItems(localTodos);
+      }
+    };
+    getLocalTodos();
+  }, []);
+
+  const filterHandler = useCallback(() => {
+    switch (status) {
+      case 'completed':
+        setFiltredTodos(items.filter((item) => item.completed === true));
+        break;
+      case 'uncompleted':
+        setFiltredTodos(items.filter((item) => item.completed === false));
+        break;
+      default:
+        setFiltredTodos(items);
+        break;
+    }
+  }, [items, status]);
+
+  React.useEffect(() => {
+    const saveLocalTodos = () => {
+      localStorage.setItem('items', JSON.stringify(items));
+    };
+    filterHandler();
+    saveLocalTodos();
+  }, [filterHandler, items, status]);
+
+  const handleInput = (e) => {
+    const value = e.target.value;
+    setCurrentItem((prevState) => ({
+      ...prevState,
+      text: value,
+      key: Date.now(),
+      completed: false,
+    }));
+  };
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = currentItem;
+    if (currentItem.text !== '') {
+      const newItems = [...items, newItem];
+      setItems(newItems);
+      setCurrentItem((prevState) => ({
+        ...prevState,
+        text: '',
+        key: '',
+      }));
+    }
+  };
+
+  const deleteItem = (key) => {
+    const filteredItems = items.filter((item) => item.key !== key);
+    setItems(filteredItems);
+  };
+
+  const setUpdate = (text, key) => {
+    setItems(
+      items.map((item) => {
+        if (item.key === key) {
+          return {
+            ...item,
+            text: text,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const setCompleted = (completed, key) => {
+    setItems(
+      items.map((item) => {
+        if (item.key === key) {
+          return {
+            ...item,
+            completed: completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <form id='to-do-form' onSubmit={addItem}>
+          <input
+            type='text'
+            value={currentItem.text}
+            onChange={handleInput}
+            placeholder='Enter your text here...'
+          />
+          <button type='submit'>Add</button>
+          <div className='form-group mt-3 pr-4'>
+            <select
+              defaultValue={status}
+              className='form-control'
+              onChange={(e) => setStatus(e.target.value)}
+            >
+              <option value='all'>All</option>
+              <option value='completed'>Completed</option>
+              <option value='uncompleted'>Uncompleted</option>
+            </select>
+          </div>
+        </form>
+        <hr style={{ backgroundColor: 'white' }} />
+        {filteredTodos.map((item) => {
+          return (
+            <ListItem
+              key={item.key}
+              item={item}
+              deleteItm={deleteItem}
+              updateItem={setUpdate}
+              setCompleted={setCompleted}
+            />
+          );
+        })}
       </header>
     </div>
   );
---
+++ src/ListItem.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './ListItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import 'animate.css/animate.css';
+const ListItem = (props) => {
+  const [isDone, setIsDone] = React.useState(props.item.completed);
+  const [isDeleted, setIsDeleted] = React.useState(false);
+  
+  return (
+    <div
+      className={
+        isDeleted
+          ? 'list-wrapper animate__animated animate__hinge'
+          : 'list-wrapper animate__animated animate__backInLeft'
+      }
+    >
+      <div className='list'>
+        <span className='checkbox'>
+          {' '}
+          <FontAwesomeIcon
+            color='#2e294e'
+            onClick={() => {
+              setIsDone(!isDone);
+              props.setCompleted(!isDone, props.item.key);
+            }}
+            icon={isDone ? faCheckSquare : faSquare}
+          />
+        </span>
+        {!isDone ? (
+          <input
+            style={{
+              backgroundColor: 'transparent',
+              border: 0,
+              color: '#2e294e',
+              outline: 'none',
+              width: '100%',
+              fontSize: 18,
+            }}
+            type='text'
+            value={props.item.text}
+            onChange={(e) => {
+              props.updateItem(e.target.value, props.item.key);
+            }}
+          />
+        ) : (
+          <span
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              opacity: 0.5,
+              textDecoration: 'line-through',
+              color: '#2e294e',
+            }}
+          >
+            {props.item.text}
+          </span>
+        )}
+        {/* {item.text} */}
+      </div>
+      <span>
+        <FontAwesomeIcon
+          color='white'
+          icon={faMinusCircle}
+          onClick={() => {
+            setIsDeleted(!isDeleted);
+            setTimeout(() => {
+              props.deleteItm(props.item.key);
+            }, 2000);
+          }}
+        />
+      </span>
+    </div>
+  );
+};
+
+export default ListItem;
